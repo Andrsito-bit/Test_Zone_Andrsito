986c028f07110c33f5db83788907094a11874cff
Andres-E <andresantosramos20@gmail.com>
2025-03-12 18:29:22 -0500

    Primer Commit

---
+++ papagallo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned short int edad;
+    cout << " ¿Cual es tu edad?";
+    cin >> edad;
+    cout << " Tu edad es " << edad ;
+    return 0;
+}
