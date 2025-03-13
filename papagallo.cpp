414f7c7dc0cec7682777dc5f43d8897cf4af82c1
Andres-E <andresantosramos20@gmail.com>
2025-03-12 19:25:27 -0500

    Implemento de eficiencia

--- papagallo.cpp
+++ papagallo.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-    unsigned short int edad;
-    cout << " ¿Cual es tu edad?";
-    cin >> edad;
-    cout << " Tu edad es " << edad ;
+int main() {
+    unsigned short n;
+
+    std::cout << "Ingrese el tamaño del array (máx 65535): ";
+    std::cin >> n;
+
+    // Reservar memoria dinámicamente
+    unsigned short* arr = new unsigned short[n];
+
+    // Llenar el array con valores
+    for (unsigned short i = 0; i < n; i++) {
+        arr[i] = i + 1; 
+    }
+
+    // Calcular la suma de los elementos
+    unsigned int suma = 0;
+    for (unsigned short i = 0; i < n; i++) {
+        suma += arr[i];
+    }
+
+    std::cout << "La suma de los elementos es: " << suma << std::endl;
+
+    // Liberar la memoria
+    delete[] arr;
+
     return 0;
 }
